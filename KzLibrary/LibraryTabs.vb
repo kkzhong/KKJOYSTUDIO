949e474849be7b011864533812f3168389c198e1
kkzhong <kkzhong@hotmail.com>
2017-07-04 13:39:37 +0800

    OFCUP

--- KzLibrary/LibraryTabs.vb
+++ KzLibrary/LibraryTabs.vb
@@ -5,9 +5,12 @@ Public Class KzTabControl
     Inherits TabControl
 
     'Dim CloseButtonClicked As Boolean
-    Dim CloseButtonBounds As Rectangle
-    Dim MouseOnTabIndex As Integer = -1
-    Dim MinTabWidth As Integer = 64
+    Dim iCloseBounds As Rectangle
+    Dim iMouseOnIndex As Integer = -1
+    Dim iMinTabWidth As Integer = 64
+    Dim sNormal As KzControlStatus = KzControlStatus.Normal
+    Dim sHover As KzControlStatus = KzControlStatus.Hover
+    Dim sSelected As KzControlStatus = KzControlStatus.Selected
 
     Public Sub Initialize()
         Me.DrawMode = TabDrawMode.OwnerDrawFixed
@@ -24,11 +27,38 @@ Public Class KzTabControl
     End Sub
 
 #Region "StyleSetting"
+    Public Property TabStyle As KzDrawingStyleCollection
+    Public Property ButtonStyle As KzDrawingStyleCollection
+    Public Sub ResetStyle()
+        Dim nor, hov, sel As KzDrawingStyle
+        Me.TabStyle = New KzDrawingStyleCollection
+        Me.ButtonStyle = New KzDrawingStyleCollection
 
+        '設定TabStyle
+        nor = New KzDrawingStyle(KzControlStatus.Normal) With {
+            .FrameWidth = 1, .FrameColor = Color.Gray}
+        hov = New KzDrawingStyle(KzControlStatus.Hover) With {
+            .FrameWidth = 0, .FrameColor = Color.Gray}
+        sel = New KzDrawingStyle(KzControlStatus.Hover) With {
+            .FrameWidth = 1, .FrameColor = Color.Black}
+
+        Me.TabStyle.AddRange({nor, hov, sel}, True)
+
+        '設定ButtonStyle
+        nor = New KzDrawingStyle(KzControlStatus.Normal) With {
+            .FrameWidth = 1, .FrameColor = Color.Gray}
+        hov = New KzDrawingStyle(KzControlStatus.Hover) With {
+            .FrameWidth = 0, .FrameColor = Color.Gray}
+        sel = New KzDrawingStyle(KzControlStatus.Hover) With {
+            .FrameWidth = 1, .FrameColor = Color.Black}
+
+        Me.ButtonStyle.AddRange({nor, hov, sel}, True)
+    End Sub
 #End Region
+
 End Class
 
-Public Enum KzStyleType
+Public Enum KzControlStatus
     None
     Normal
     Hover
@@ -40,14 +70,70 @@ Public Enum KzStyleType
     Zooming
 End Enum
 
-Public Class KzDrawingSet
+Public Class KzDrawingStyleCollection
     Inherits List(Of KzDrawingStyle)
 
-    Public Sub New()
-
+    Public Overloads Sub AddRange(styles As IEnumerable(Of KzDrawingStyle), replace As Boolean)
+        For Each style In styles
+            Me.Add(style, replace)
+        Next
     End Sub
 
-    Public Property Status As KzStyleType = KzStyleType.None
+    Public Overloads Sub Add(item As KzDrawingStyle, replace As Boolean)
+        Dim go As Boolean = True
+        Dim rs As KzDrawingStyle = GetStyle(item.Type)
+
+        If Not rs.Equals(Nothing) Then
+            If replace Then
+                Me.Remove(rs)
+            Else
+                go = False
+            End If
+        End If
+
+        'If Me.Count > 0 Then
+        '    For Each style As KzDrawingStyle In Me
+        '        If style.Type = item.Type And
+        '            item.Type <> KzControlStatus.None Then
+
+        '            If replace Then
+        '                Me.Remove(style)
+        '            Else
+        '                go = False
+        '            End If
+        '            Exit For
+        '        End If
+        '    Next
+        'End If
+
+        If go Then
+            Me.Add(item)
+        End If
+    End Sub
+
+    Public Overloads Function Contains(styleStatus As KzControlStatus) As Boolean
+        For Each item As KzDrawingStyle In Me
+            If item.Type = styleStatus Then
+                Return True
+                Exit For
+            End If
+        Next
+
+        Return False
+    End Function
+
+    Public Function GetStyle(status As KzControlStatus) As KzDrawingStyle
+        If Me.Count > 0 Then
+            For Each item As KzDrawingStyle In Me
+                If item.Type = status Then
+                    Return item
+                    Exit For
+                End If
+            Next
+        End If
+
+        Return Nothing
+    End Function
 End Class
 
 Public Structure KzDrawingStyle
@@ -56,10 +142,8 @@ Public Structure KzDrawingStyle
     Dim iFSize As Single
     Dim iFStyle As FontStyle
 
-    Public Sub New(type As KzStyleType, Optional styleSet As KzDrawingSet = Nothing)
-        Me.Parent = styleSet
-        Me.StyleType = type
-
+    Public Sub New(type As KzControlStatus)
+        Me.Type = type
         Me.FrameWidth = 0
         Me.FrameColor = SystemColors.ActiveBorder
         Me.InnerLineWidth = 1
@@ -67,15 +151,18 @@ Public Structure KzDrawingStyle
         Me.FillColor = SystemColors.Control
         Me.FontColor = SystemColors.ControlText
         Me.Font = SystemFonts.DefaultFont
+        Me.ShadowWidth = 0
+        Me.ShadowFillColor = SystemColors.ButtonShadow
     End Sub
 
-    Public Property Parent As KzDrawingSet
-    Public Property StyleType As KzStyleType
+    Public Property Type As KzControlStatus
     Public Property FrameWidth As Integer
     Public Property FrameColor As Color
     Public Property InnerLineWidth As Integer '= 1
     Public Property InnerLineColor As Color '= SystemColors.ControlText
     Public Property FillColor As Color '= SystemColors.Control
+    Public Property ShadowWidth As Integer
+    Public Property ShadowFillColor As Color
     Public Property FontColor As Color
     Public Property Font As Font '= SystemFonts.DefaultFont
         Get
@@ -140,6 +227,11 @@ Public Structure KzDrawingStyle
     Public ReadOnly Property FontBrush As Brush
         Get
             Return New SolidBrush(Me.FontColor)
+        End Get
+    End Property
+    Public ReadOnly Property ShadowFillBrush As Brush
+        Get
+            Return New SolidBrush(Me.ShadowFillColor)
         End Get
     End Property
 End Structure 'KzDrawingStyle
